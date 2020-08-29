8de0b49afb7e8772af61067e1fd13e690f408cd3
jingechen <cjg_free@163.com>
2020-08-29 20:43:49 +0800

    add:支持相同路径,多个回调

--- test/2.router.js
+++ test/2.router.js
@@ -7,9 +7,10 @@ const app = express();
  */
 app.get('/', function (req, res, next) {
     console.log(1);
+    // 如果任何出错了, 会把错误交给next, 然后会跳过后面所有的正常处理函数, 交给错误中间件来处理。
     next();
 }, function (req, res, next) {
-    console.log(11);
+    console.log(11); // 为何111 打印不出来??????
     next();
 })
 app.get('/2', function (req, res, next) {
@@ -20,4 +21,4 @@ app.get('/', function (req, res, next) {
     console.log(3);
     res.end('ok');
 });
-app.listen(3000);
+app.listen(3000, () => console.log("port at 3000"));
